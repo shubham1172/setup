f6ff38a9e2af01bdbe9445803be50c7ec80b9b58
Shubham Sharma <shubhamsharma1172@gmail.com>
2019-07-03 16:32:46 +0530

    $timestamp

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
 # This script helps me update my dot files regularly
-# It exists in my crontab as:
+# It exists in my anacrontab as:
 #
-# */30 * * * * ~/setup/scripts/backup.sh >/dev/null 2>&1
+# @daily  10      setup.backup            su -c /home/shubham1172/setup/scripts/backup.sh - shubham1172 >> /home/shubham1172/.anacronlogs 2>&1
+
+cd ${HOME}/setup/
 
 BACKUP=${HOME}/setup/scripts/backup
+
+timestamp() {
+  date +"%m-%d-%Y"
+}
 
 grep -v '^$\|^\s*\#' $BACKUP| while read -r line; do
 	echo "$line" | awk '{ system("rsync " $1 " " $2) }'
 done
+
+if [[ `git status --porcelain` ]]; then
+	git add .
+	git commit -m '$timestamp'
+	git push origin master
+fi
